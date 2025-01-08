967af4d1145736e4d6ed7f12046bcab07c3e7fc4
Ekta Mehra <mehra.ekta95@gmail.com>
2025-01-08 12:59:36 +0000

    added comment CRUD

--- about/admin.py
+++ about/admin.py
@@ -1,9 +1,34 @@
-from django.contrib import admin
+from django.shortcuts import render
+from django.contrib import messages
 from .models import About
-from django_summernote.admin import SummernoteModelAdmin
+from .forms import CollaborateForm
+# Create your views here.
 
 
-@admin.register(About)
-class AboutAdmin(SummernoteModelAdmin):
+def about_me(request):
+    """
+    Renders the About page
+    """
+    about = About.objects.all().order_by('-updated_on').first()
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_request = collaborate_form.save(commit=False)
+            collaborate_request.read = False
+            collaborate_request.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Collaboration request received! I endeavor to respond within 2 working days.'
+            )
+            collaborate_form = CollaborateForm()
+        else:
+            collaborate_form = CollaborateForm()
+    else:
+        collaborate_form = CollaborateForm()
 
-    summernote_fields = ('content',)
+    return render(
+        request,
+        "about/about.html",
+        {"about": about,
+        "collaborate_form": collaborate_form},
+    )
