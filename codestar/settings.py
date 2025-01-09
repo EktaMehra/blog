add839d9370f1498dcf6ed8dce2ddc7ce30cc07e
Ekta Mehra <mehra.ekta95@gmail.com>
2025-01-09 10:41:05 +0000

    enable serving of image files

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-ektamehra-blog-gq322l144ge.ws.codeinstitute-ide.net','.herokuapp.com']
 
@@ -51,6 +51,8 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'crispy_forms',
     'crispy_bootstrap5',
+    'cloudinary_storage',
+    'cloudinary',
 ]
 
 SITE_ID = 1
