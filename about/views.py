f52dd01b55ed0317c4524c3adc4388fa74129f84
Ekta Mehra <mehra.ekta95@gmail.com>
2025-01-09 14:07:09 +0000

    add changes to urls.py

--- about/views.py
+++ about/views.py
@@ -35,8 +35,6 @@ def about_me(request):
     collaborate_form = CollaborateForm()
 
     return render(
-        request,
-        "about/about.html",
-        {"about": about,
-         "collaborate_form": collaborate_form},
+        request, "about/about.html",
+        {"about": about, "collaborate_form": collaborate_form},
     )
